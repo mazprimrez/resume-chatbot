598cb6f7ccd4623ed943e6e28eb7c52553a3f398
Mazi Prima Reza <33874473+mazprimrez@users.noreply.github.com>
2024-06-29 17:44:45 +0700

    add requirements and adjustment

--- utils/utils.py
+++ utils/utils.py
@@ -18,7 +18,23 @@ def inference(client, question: str, context: str):
             Question: {question}"""
 
     messages = [
-    {"role": "system", "content": "You are a helpful assisstent that knows everything about Mazi Prima Reza. She's a female. Using the information contained in the context, give a detailed answer in 1 to 3 sentence to the question. Answer in the same language as the question's language but don't translate technical terms."},
+    {"role": "system", "content": "You are a helpful assisstent that knows everything about Mazi Prima Reza. She's a female. Using the information contained in the context, give a detailed answer in 1 to 5 sentences to the question. Answer in the same language as the question's language but don't translate technical terms."},
+    {"role": "user", "content": """
+                    Context: Notable Projects at Vidio:  
+                        • Recommendation System : Designed and implemented a collaborative filtering 
+                        recommendation system using cosine similarity, which improved user engagement 
+                        metrics significantly.  
+                        the finance sector to summarize and analyze new and updated 
+                        regulat ions, thereby aiding GRC analysts in streamlining their workflow and automating 
+                        analysis to inform the team faster.  
+                        Notable Projects at Metrodata:  
+                        • HR Chatbot Development : Led the development of a chatbot using Microsoft Azure 
+                        Open AI for the HR department.  This project involved integrating NLP models to 
+                        answer employee queries about company policies, resulting in a 30% reduction in HR 
+                        response time.  
+                        • Regulation Analysis Tool : Developed a Generative AI tool for the finance industry 
+                    Question: Can you tell me about a specific project at Vidio?"""},
+    {"role": "assistant", "content":"At Vidio, she developed a recommendation model using collaborative filtering with cosine similarity, achieving a notable 0.2 Recall@20 and a 10%+ click-through rate, surpassing in-house model-based recommendation systems."},
     {"role": "user", "content": prompt},
     ]
 
